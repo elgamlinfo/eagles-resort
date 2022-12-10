fbd871bcc7e8a34c59cc52530ddee90c51c76bff
elgamlinfo <elgaml.info@gmail.com>
2022-12-10 13:48:04 +0200

    fix images

--- src/Pages/SplashScreen/SplashScreen.jsx
+++ src/Pages/SplashScreen/SplashScreen.jsx
@@ -6,12 +6,12 @@ const SplashScreen = () => {
   return (
     <div className='splash_screen'>
       <video autoPlay loop muted playsInline height={"100%"}  className='back_video'>
-         <source src='images/splash.mp4' type='video/mp4'/>
+         <source src='/images/splash.mp4' type='video/mp4'/>
       </video>
       <div className='overlay'>
         <div className='content'>
           <div className='logo'>
-            <img src="images/logo.png" alt='' />
+            <img src="/images/logo.png" alt='' />
             <h1>eagles resort</h1>
           </div>
           <div className='lang_btns'>
