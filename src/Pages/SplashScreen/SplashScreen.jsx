ba848037acdab7dc26c05f06f7fcdb210ad0a783
elgamlinfo <elgaml.info@gmail.com>
2022-12-10 14:08:45 +0200

    fix images

--- src/Pages/SplashScreen/SplashScreen.jsx
+++ src/Pages/SplashScreen/SplashScreen.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './style.scss'
-
-
+import video from "../../assets/splash.mp4"
+import logo from '../../assets/logo.png'
 const SplashScreen = () => {
   return (
     <div className='splash_screen'>
       <video autoPlay loop muted playsInline height={"100%"}  className='back_video'>
-         <source src='/images/splash.mp4' type='video/mp4'/>
+         <source src={video} type='video/mp4'/>
       </video>
       <div className='overlay'>
         <div className='content'>
           <div className='logo'>
-            <img src="/images/logo.png" alt='' />
+            <img src={logo} alt='' />
             <h1>eagles resort</h1>
           </div>
           <div className='lang_btns'>
